723856fe506a719e8f51c07ec153d3c046b169de
00sg <85273791+00sg@users.noreply.github.com>
2021-07-02 23:36:42 +0530

    Add files via upload

---
+++ components/SidebarOptions.js
@@ -0,0 +1,42 @@
+import { Add } from "@material-ui/icons";
+import React from "react";
+import "./SidebarOption.css";
+
+function SidebarOptions() {
+  return (
+    <div className="sidebarOptions">
+      <div className="sidebarOption">
+     
+       <img
+          /* src="https://qphs.fs.quoracdn.net/main-thumb-t-930-100-cbbsbwijdhpyzlpipejvqpiijhhoaday.jpeg"
+          alt=""*/
+        />
+        <p>MY PROFILE</p>
+      </div>
+
+      <div className="sidebarOption">
+        <img
+        /*
+          src="https://qphs.fs.quoracdn.net/main-thumb-t-858-100-VnZbEVtOIGkEHXlnYId9slumV59IPgkA.jpeg"
+          alt=""*/
+        />
+
+        <p>FOLLOWING</p>
+      </div>
+      <div className="sidebarOption">
+        <img
+        /*
+          src="https://qphs.fs.quoracdn.net/main-thumb-t-1913-100-B8JrwaVauFzsaTSqXDqoWLCXzQb2mTE9.jpeg"
+          alt=""*/
+        />
+        <p>ABOUT US</p>
+        </div>
+      <div className="sidebarOption">
+        <Add />
+        <p className="text">Discover Spaces</p>
+      </div>
+    </div>
+  );
+}
+
+export default SidebarOptions;
